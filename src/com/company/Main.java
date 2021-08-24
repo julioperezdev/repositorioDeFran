fd28d431e600519ec3eed9ebae34f0992bf9dc66
julioperezdev <protobot.dev@gmail.com>
2021-08-23 21:31:20 -0300

    creando una suma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("primer hola mundo");
+
+        int numero1=5;
+        int numero2=8;
+        int resultado= numero1+numero2;
+        System.out.println("el resultado de la suma es ".concat(String.valueOf(resultado)));
+
     }
 }
